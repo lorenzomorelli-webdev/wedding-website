83a53d5574936e8f478f84f9813f40d49901b962
Lorenzo Morelli <lorenzomorelli95@gmail.com>
2024-11-27 15:58:17 +0100

    fixed mobile vh and settle up google calendar + interaction with google sheet and mail to store invitation

--- js/scripts.js
+++ js/scripts.js
@@ -219,11 +219,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'defa50a7babc2b727c44fe4e03905bf4') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzV8QaZ89hycN5io8XXgaoUiiQd8RJYk67w3QxdNVnp0_hd5aUlEsV7jBnr5zJ4nxTV/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
